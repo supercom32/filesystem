98f910452927b087722509384b1344959b1ea70f
Alex Chang <supercom32@gmail.com>
2021-10-04 01:06:02 -0400

    Reverted some comments for testing purposes.

--- filesystem.go
+++ filesystem.go
@@ -405,8 +405,7 @@ func GetListOfDirectoryContents(directoryPath string, regexMatchers []string, is
 /**
 FindMatchingContent allows you to find matching content from a given directory
 path. Both shallow and recursive searches are supported and results are
-returned as a fully qualified path. In the event an error occurs, matching will stop
-and the error will be returned.
+returned as a fully qualified path.
 */
 func FindMatchingContent(directoryPath string, regexMatchers []string, isFilesIncluded bool, isDirectoriesIncluded bool, isRecursive bool) ([]string, error) {
 	var err error
