306905a73a2171db8d4032d1f7fcd84ccbb375fd
administrator <your.email@example.com>
2025-03-24 21:24:29 -0400

    small fixes.

--- filesystem.go
+++ filesystem.go
@@ -12,6 +12,7 @@ import (
 	"regexp"
 	"runtime"
 	"strings"
+	"time"
 )
 
 type fileInstanceType struct {
@@ -31,7 +32,7 @@ Open allows you to access a file on the file system in the open state.
 */
 func (shared *fileInstanceType) Open(fileName string, permissions int) error {
 	if permissions == 0 {
-		permissions = 0644
+		permissions = 0744
 	}
 	perm := os.FileMode(uint32(permissions))
 	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, perm)
@@ -131,13 +132,13 @@ func (shared *fileInstanceType) GetFirstLine() ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return firstLine[:len(firstLine) - 1], err // Remove trailing delimiter "\n"
+	return firstLine[:len(firstLine)-1], err // Remove trailing delimiter "\n"
 }
 
 /*
 RemoveFirstLine allows you to remove the first line from a text file.
 */
-func (shared *fileInstanceType) RemoveFirstLine() error{
+func (shared *fileInstanceType) RemoveFirstLine() error {
 	fileInfo, err := shared.fileDescriptor.Stat()
 	if err != nil {
 		return err
@@ -226,13 +227,18 @@ func FindReplaceInFile(filename string, regexMatcher string, replacementValue st
 	if err != nil {
 		return err
 	}
+	fileContentString := string(fileContents)
+	lines := strings.Split(fileContentString, "\n")
 	regex := regexp.MustCompile(regexMatcher)
-	newContents := regex.ReplaceAll(fileContents, []byte(replacementValue))
+	for i, line := range lines {
+		lines[i] = regex.ReplaceAllString(line, replacementValue)
+	}
+	newContents := []byte(strings.Join(lines, "\n"))
 	err = ioutil.WriteFile(filename, newContents, 0)
 	if err != nil {
 		return err
 	}
-	return err
+	return nil
 }
 
 /*
@@ -244,7 +250,7 @@ func GetLastLineFromFile(fileName string) (string, error) {
 		return "", err
 	}
 	arrayOfLines := strings.Split(string(fileContents), "\n")
-	return arrayOfLines[len(arrayOfLines) - 1], err
+	return arrayOfLines[len(arrayOfLines)-1], err
 }
 
 /*
@@ -267,7 +273,7 @@ func GetFileContents(fileName string) ([]byte, error) {
 /*
 RemoveFirstLineFromFile allows you to remove the first line from a file.
 */
-func RemoveFirstLineFromFile(fileName string) (error) {
+func RemoveFirstLineFromFile(fileName string) error {
 	file := GetFileInstance()
 	err := file.Open(fileName, 0)
 	if err != nil {
@@ -281,8 +287,9 @@ func RemoveFirstLineFromFile(fileName string) (error) {
 	return err
 }
 
-/**
-DownloadFile allows you to download a file from the internet to your local file
+/*
+*
+DownloadFile allows you to download a file from the internet to your local.com file
 system.
 */
 func DownloadFile(url string, filepath string, header http.Header) error {
@@ -311,7 +318,8 @@ func DownloadFile(url string, filepath string, header http.Header) error {
 	return err
 }
 
-/**
+/*
+*
 CopyFile allows you to copy a file from one source location to a target
 destination location. In the event the operation could not be completed,
 an error is returned to the user.
@@ -339,26 +347,28 @@ func CopyFile(sourceFile string, destinationFile string) error {
 	return err
 }
 
-/**
+/*
+*
 WriteBytesToFile allows you to write a series of bytes to a given file.
 In addition, the following information should be noted:
 
 - In the event the file does not already exist, it will be created for you
 with the permission attributes provided.
 
-- If you pass in a permissions value of '0', the default value of 666 will
+- If you pass in a permissions value of '0', the default value of 744 will
 be used instead.
 */
 func WriteBytesToFile(fileName string, bytesToWrite []byte, permissions int) error {
 	if permissions == 0 {
-		permissions = 666
+		permissions = 0744
 	}
 	perm := os.FileMode(uint32(permissions))
 	err := ioutil.WriteFile(fileName, bytesToWrite, perm)
 	return err
 }
 
-/**
+/*
+*
 GetFileContentsAsBytes allows you to get the contents of a file as a byte
 array.
 */
@@ -372,19 +382,20 @@ func GetFileContentsAsBytes(fileName string) ([]byte, error) {
 	return fileContents, err
 }
 
-/**
+/*
+*
 AppendLineToFile allows you to append a line to the end of a file.
 In addition, the following information should be noted:
 
 - In the event the file does not already exist, it will be created for you
 with the permission attributes provided.
 
-- If you pass in a permissions value of '0', the default value of 666 will
+- If you pass in a permissions value of '0', the default value of 744 will
 be used instead.
 */
 func AppendLineToFile(fileName string, lineToWrite string, permissions int) error {
 	if permissions == 0 {
-		permissions = 0644
+		permissions = 0744
 	}
 	perm := os.FileMode(uint32(permissions))
 	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, perm)
@@ -393,15 +404,17 @@ func AppendLineToFile(fileName string, lineToWrite string, permissions int) erro
 	return err
 }
 
-/**
+/*
+*
 GetListOfFiles allows you to obtain a list of files that match a given regular
 expression.
 */
 func GetListOfFiles(directoryPath string, regexMatcher string) ([]string, error) {
-	return GetListOfDirectoryContents(directoryPath, []string{regexMatcher},true, false)
+	return GetListOfDirectoryContents(directoryPath, []string{regexMatcher}, true, false)
 }
 
-/**
+/*
+*
 GetListOfDirectories allows you to obtain a list of files that match a given
 regular expression.
 */
@@ -425,7 +438,8 @@ func IsDirectory(directoryPath string) bool {
 	return false
 }
 
-/**
+/*
+*
 GetListOfDirectoryContents allows you to obtain a list of files and directories
 that match a given regular expression.
 */
@@ -442,12 +456,12 @@ func GetListOfDirectoryContents(directoryPath string, regexMatchers []string, is
 			match := regex.FindStringSubmatch(file.Name())
 			if len(match) > 0 {
 				if file.IsDir() && isDirectoriesIncluded {
-					fileList = append(fileList, file.Name() + "/")
-					break;
+					fileList = append(fileList, file.Name()+"/")
+					break
 				}
 				if !file.IsDir() && isFilesIncluded {
 					fileList = append(fileList, file.Name())
-					break;
+					break
 				}
 			}
 		}
@@ -455,7 +469,8 @@ func GetListOfDirectoryContents(directoryPath string, regexMatchers []string, is
 	return fileList, err
 }
 
-/**
+/*
+*
 FindMatchingContent allows you to find matching content from a given directory
 path. Both shallow and recursive searches are supported and results are
 returned as a fully qualified path.
@@ -492,7 +507,8 @@ func FindMatchingContent(directoryPath string, regexMatchers []string, isFilesIn
 	return listOfContents, err
 }
 
-/**
+/*
+*
 addPrefixToStrings allows you to append a prefix to an array of strings.
 */
 func addPrefixToStrings(prefixToAdd string, stringArray []string) []string {
@@ -502,14 +518,15 @@ func addPrefixToStrings(prefixToAdd string, stringArray []string) []string {
 	return stringArray
 }
 
-/**
+/*
+*
 GetNormalizedDirectoryPath allows you to guarantee that a directory path
 is always formatted with a trailing slash at the end. This is useful when
 you need to work with paths in predictable and consistent manner.
- */
+*/
 func GetNormalizedDirectoryPath(directoryPath string) string {
 	normalizedDirectoryPath := directoryPath
-	if !strings.HasSuffix(directoryPath, "/")  && !strings.HasSuffix(directoryPath, "\\") {
+	if !strings.HasSuffix(directoryPath, "/") && !strings.HasSuffix(directoryPath, "\\") {
 		normalizedDirectoryPath = normalizedDirectoryPath + "/"
 	}
 	return normalizedDirectoryPath
@@ -522,14 +539,15 @@ formatted this way.
 */
 func GetBareDirectoryPath(directoryPath string) string {
 	bareDirectoryPath := directoryPath
-	if strings.HasSuffix(directoryPath, "/")  || strings.HasSuffix(directoryPath, "\\") {
+	if strings.HasSuffix(directoryPath, "/") || strings.HasSuffix(directoryPath, "\\") {
 		bareDirectoryPath = strings.TrimSuffix(directoryPath, "/")
 		bareDirectoryPath = strings.TrimSuffix(bareDirectoryPath, "\\")
 	}
 	return bareDirectoryPath
 }
 
-/**
+/*
+*
 IsDirectoryEmpty allows you to detect if a directory is empty or not.
 */
 func IsDirectoryEmpty(directoryName string) (bool, error) {
@@ -545,10 +563,11 @@ func IsDirectoryEmpty(directoryName string) (bool, error) {
 	return false, err
 }
 
-/**
+/*
+*
 GetFileSize allows you to obtain the size of a specified file in bytes.
 */
-func GetFileSize(fileName string) (int64, error){
+func GetFileSize(fileName string) (int64, error) {
 	var fileSize int64
 	file, err := os.Stat(fileName)
 	if err != nil {
@@ -558,7 +577,8 @@ func GetFileSize(fileName string) (int64, error){
 	return fileSize, err
 }
 
-/**
+/*
+*
 GetWorkingDirectory allows you to obtain the current working directory
 where your program is executing.
 */
@@ -572,7 +592,8 @@ func GetWorkingDirectory() (string, error) {
 	return parent, err
 }
 
-/**
+/*
+*
 GetParentDirectory allows you to obtain the container directory of
 your provided path. This will be everything except the last element
 of your path.
@@ -586,8 +607,9 @@ func GetParentDirectory(fileOrDirectoryPath string) string {
 	return parentDirectory
 }
 
-/**
-RenameFile allows you to rename a file on your local file system. In the event
+/*
+*
+RenameFile allows you to rename a file on your local.com file system. In the event
 that a file with the same name already exists, it will be overwritten. Here we
 explicitly do the delete so we don't depend on the 'os.Rename' behaviour of
 overwriting files which may be environment dependant.
@@ -602,11 +624,11 @@ func RenameFile(sourceFileName string, targetFileName string) error {
 	// Since Windows is case insensitive we check if the names are identical, we
 	// give the file a temporary name before we request to rename it properly.
 	if runtime.GOOS == "windows" && strings.ToLower(bareSourceFileName) == strings.ToLower(bareTargetFileName) {
-		err = os.Rename(bareSourceFileName, bareTargetFileName + ".tmp")
+		err = os.Rename(bareSourceFileName, bareTargetFileName+".tmp")
 		if err != nil {
 			return err
 		}
-		err = os.Rename(bareTargetFileName + ".tmp", bareTargetFileName)
+		err = os.Rename(bareTargetFileName+".tmp", bareTargetFileName)
 		return err
 	}
 	// This needs to be here to avoid deleting target files on Windows before a case-insensitive check is done.
@@ -617,7 +639,8 @@ func RenameFile(sourceFileName string, targetFileName string) error {
 	return err
 }
 
-/**
+/*
+*
 IsDirectoryExists allows you to check if a directory exists or not on the file
 system.
 */
@@ -625,14 +648,16 @@ func IsDirectoryExists(directoryPath string) bool {
 	return isDiskEntryExists(directoryPath)
 }
 
-/**
+/*
+*
 IsFileExists allows you to check if a file exists or not on the file system.
 */
 func IsFileExists(filePath string) bool {
 	return isDiskEntryExists(filePath)
 }
 
-/**
+/*
+*
 isDiskEntryExists allows you to check if a valid disk entry exists on the
 file system.
 */
@@ -644,7 +669,16 @@ func isDiskEntryExists(path string) bool {
 	return true
 }
 
-/**
+func GetAbsolutePath(filePath string) (string, error) {
+	absPath, err := filepath.Abs(filePath)
+	if err != nil {
+		return "", err
+	}
+	return absPath, nil
+}
+
+/*
+*
 DeleteFile allows you to delete a file on the file system.
 */
 func DeleteFile(fileName string) error {
@@ -652,11 +686,12 @@ func DeleteFile(fileName string) error {
 	return err
 }
 
-/**
+/*
+*
 DeleteFilesMatchingPattern allows you to delete files matching a specific
 pattern. Pattern syntax is the same as the 'Match' command.
 */
-func DeleteFilesMatchingPattern(fileName string ) error {
+func DeleteFilesMatchingPattern(fileName string) error {
 	files, err := filepath.Glob(fileName)
 	if err != nil {
 		return err
@@ -669,7 +704,6 @@ func DeleteFilesMatchingPattern(fileName string ) error {
 	return err
 }
 
-
 /*
 DeleteDirectory allows you to recursively remove a directory from the file
 system.
@@ -679,18 +713,54 @@ func DeleteDirectory(pathName string) error {
 	return err
 }
 
-
-/**
+/*
+*
 MoveFile allows you to move a file from one location to another. This method
 is an alias which simply performs a rename command, which is capable of doing
 the same action.
 */
 func MoveFile(sourceFile string, destinationFile string) error {
-	err := RenameFile(sourceFile, destinationFile)
+	// Since windows is case-insensitive, it is valid if the user wants to rename / move a file to the
+	// same location, since he is just trying to change case. So we only error out if this edge-case is not detected.
+	if runtime.GOOS != "windows" && sourceFile != destinationFile && IsFileExists(destinationFile) {
+		return errors.New(fmt.Sprintf("Cannot move '%s' to the destination location since '%s' already exists .", sourceFile, destinationFile))
+	}
+	if runtime.GOOS == "windows" && strings.ToLower(sourceFile) != strings.ToLower(destinationFile) && IsFileExists(destinationFile) {
+		return errors.New(fmt.Sprintf("Cannot move '%s' to the destination location since '%s' already exists .", sourceFile, destinationFile))
+	}
+	now := time.Now()
+	uniqueId := fmt.Sprintf("%d", now.Unix())
+	uniqueFileName := destinationFile + "_" + uniqueId
+	err := RenameFile(sourceFile, uniqueFileName)
+	if err != nil {
+		return err
+	}
+	err = RenameFile(uniqueFileName, destinationFile)
 	return err
 }
 
-/**
+func MoveDirectories(sourceDir string, destinationDir string) error {
+	normalizedSourceDirectory := strings.TrimRight(sourceDir, "\\/")
+	normalizedDestinationDirectory := strings.TrimRight(destinationDir, "\\/")
+	if runtime.GOOS != "windows" && sourceDir != destinationDir && IsDirectoryExists(destinationDir) {
+		return errors.New(fmt.Sprintf("Cannot move '%s' to the destination location since '%s' already exists.", sourceDir, destinationDir))
+	}
+	if runtime.GOOS == "windows" && strings.ToLower(sourceDir) != strings.ToLower(destinationDir) && IsDirectoryExists(destinationDir) {
+		return errors.New(fmt.Sprintf("Cannot move '%s' to the destination location since '%s' already exists.", sourceDir, destinationDir))
+	}
+
+	now := time.Now()
+	uniqueId := fmt.Sprintf("%d", now.Unix())
+	uniqueDirName := normalizedDestinationDirectory + "_" + uniqueId
+	err := os.Rename(normalizedSourceDirectory, uniqueDirName)
+	if err != nil {
+		return err
+	}
+	err = os.Rename(uniqueDirName, normalizedDestinationDirectory)
+	return err
+}
+
+/*
 CreateDirectory allows you to create a directory on your local file system.
 */
 func CreateDirectory(directoryPath string, permissions uint32) error {
@@ -702,7 +772,7 @@ func CreateDirectory(directoryPath string, permissions uint32) error {
 	return err
 }
 
-/**
+/*
 GetFileExtension allows you to get the file extension of a file.
 */
 func GetFileExtension(fileName string) string {
@@ -710,7 +780,7 @@ func GetFileExtension(fileName string) string {
 	return extension
 }
 
-/**
+/*
 GetBaseFileName allows you to extract the base name of a file without any path or
 file extensions.
 */
@@ -720,7 +790,7 @@ func GetBaseFileName(fileName string) string {
 	return strings.TrimSuffix(normalizedFileName, filepath.Ext(normalizedFileName))
 }
 
-/**
+/*
 GetBaseDirectory allows you to extract the directory from a file path.
 */
 func GetBaseDirectory(filePath string) string {
@@ -739,18 +809,17 @@ func GetCurrentDirectory(directoryPath string) string {
 	return currentDirectory
 }
 
-
-/**
+/*
 GetFileNameFromPath allows you to extract a file name from a fully qualified file path.
 */
 func GetFileNameFromPath(fullyQualifiedFileName string) string {
 	return filepath.Base(fullyQualifiedFileName)
 }
 
-/**
+/*
 IsFile allows you to check if an item on the file system is a file or not.
 */
-func IsFile(path string) (bool, error){
+func IsFile(path string) (bool, error) {
 	var isFile bool
 	fi, err := os.Stat(path)
 	if err != nil {
@@ -761,4 +830,16 @@ func IsFile(path string) (bool, error){
 		isFile = true
 	}
 	return isFile, err
+}
+
+/*
+GetAbsolutePathToExecutableLocation allows you to obtain an absolute path to your currently running executable.
+*/
+func GetAbsolutePathToExecutableLocation() (string, error) {
+	executablePath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	executableDir := filepath.Dir(executablePath)
+	return executableDir, nil
 }
